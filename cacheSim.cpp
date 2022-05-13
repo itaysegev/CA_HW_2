2f68cc1df17bb17a0c3d6bc14d68b6ce289ed835
Itay Segev <itaysegev11@gmail.com>
2022-05-13 15:30:52 +0300

    changes

--- cacheSim.cpp
+++ cacheSim.cpp
@@ -70,6 +70,7 @@ inline int blocksNumCalc(unsigned int BSize, unsigned int DSize) {
 int setCalc(string hex_address, int assoc) {
 	string bin_address = hexToBin(hex_address);
 	string set_bin = bin_address.substr(2, assoc);
+	cout << set_bin << endl;
 	return strtoul(set_bin.c_str(), NULL, 2); 
 }
 
