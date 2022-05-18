dd00797995d5b4421d5d1cfc7a486c3ee68d18be
Itay Segev <itaysegev11@gmail.com>
2022-05-18 12:24:11 +0300

    first version

--- cacheSim.cpp
+++ cacheSim.cpp
@@ -312,7 +312,7 @@ int main(int argc, char **argv) {
 		cout << ", address (hex)" << cutAddress << endl;
 		unsigned long int num = 0;
 		num = strtoul(cutAddress.c_str(), NULL, 16);
-		char R = 'R'
+		char R = 'R';
 		if(operation == R) {
 			mem.read(cutAddress);
 		}
